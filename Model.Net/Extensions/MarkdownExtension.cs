fad30ba22aef9c5b15eabf07101992c24b575d99
Sergi Gisbert <sgisbert@users.noreply.github.com>
2020-11-23 11:43:00 +0100

    Model library for extensions and other stuff

---
+++ Model.Net/Extensions/MarkdownExtension.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using System;
+using System.IO;
+
+namespace Model.Extensions
+{
+    public static class MarkdownExtension
+    {
+        #region "Métodos"
+
+        public static IHtmlContent RenderMarkdown(this IHtmlHelper helper, string filename)
+        {
+            var rootPath = (string)AppDomain.CurrentDomain.GetData("ContentRootPath");
+            //var selectedLanguage = helper.ViewContext.Controller.ControllerContext.GetSelectedLanguage();
+
+            //var pathSelected = helper.ViewContext.HttpContext.Server.MapPath(filename + "." + selectedLanguage + ".md");
+            var pathDefault = Path.Combine(rootPath, filename.TrimStart('/').Replace("/", @"\\") + ".md");
+
+            //if (!File.Exists(pathSelected))
+            if (!File.Exists(pathDefault))
+            {
+                return helper.Raw("<div class=\"alert alert-danger fade in\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button><i class=\"icon-warning-sign\"></i> File " + pathDefault + " not found</div>");
+            }
+            //else
+            //    pathSelected = pathDefault;
+
+            //string cacheKey = pathSelected;
+            string cacheKey = pathDefault;
+
+            // Load source text
+            //var text = Cache.Get<String>(cacheKey, makeCopy: false);
+            //if (text == null || HttpContext.Current.IsDebuggingEnabled)
+            //{
+                //text = File.ReadAllText(pathSelected);
+                var text = File.ReadAllText(pathDefault);
+            //    Cache.Insert(cacheKey, text, null, CacheConstants.DefaultExpirationTime, makeCopy: false);
+            //}
+
+            // Setup processor
+            var md = new MarkdownDeep.Markdown
+            {
+                SafeMode = false,
+                ExtraMode = true,
+                AutoHeadingIDs = true,
+                MarkdownInHtml = true,
+                NewWindowForExternalLinks = true
+            };
+
+            // Write it
+            return helper.Raw(md.Transform(text));
+        }
+
+        #endregion
+
+    }
+
+}
