21b46adb59bd6f90243cba0de4dccac24271b168
Sergi Gisbert <sgisbert@users.noreply.github.com>
2020-11-26 13:17:09 +0100

    Blog list and post page, initial version

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -4,9 +4,9 @@ namespace Miniblog.Core
     {
         public int CommentsCloseAfterDays { get; set; } = 10;
 
-        public PostListView ListView { get; set; } = PostListView.TitlesAndExcerpts;
+        public PostListView ListView { get; set; } = PostListView.FullPosts;
 
-        public string Owner { get; set; } = "The Owner";
+        public string Owner { get; set; } = "Sergi Gisbert";
 
         public int PostsPerPage { get; set; } = 4;
     }
